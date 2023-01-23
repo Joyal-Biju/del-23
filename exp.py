ec87b42d1781f672537685af624e567605e0dd36
Joyal-Biju <123347008+Joyal-Biju@users.noreply.github.com>
2023-01-23 10:06:00 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,4 +1,4 @@
 org = 'deloitte'
 id = 'batch A'
 
-print('welcome to cdf class conducted by () and belong to ()).format(org,id)
+print('welcome to cdf class conducted by {} and belong to {}).format(org,id)
